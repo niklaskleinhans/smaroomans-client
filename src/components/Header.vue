10ecfcada879f4870c2e4f8fd820f0d7056eb4a2
Marius Altmann <dodeca@gmx.de>
2019-06-29 03:50:33 +0200

    added title to nav bar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,16 @@
 <template>
   <v-toolbar>
+    <v-toolbar-title>Smart Room Management System</v-toolbar-title>
+    <v-spacer></v-spacer>
     <v-toolbar-items>
       <v-btn flat>
         <router-link to="/">User</router-link>
       </v-btn>
-      <v-spacer></v-spacer>
       <v-btn flat>
         <router-link to="/system">System</router-link>
       </v-btn>
     </v-toolbar-items>
+    <v-spacer></v-spacer>
   </v-toolbar>
 </template>
 
